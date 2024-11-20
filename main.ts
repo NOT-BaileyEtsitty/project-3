074dddbb5d3326f20547002a4680c931223b4fe4
NOT-BaileyEtsitty <betsitt@gilbertschools.net>
2024-11-20 08:07:50 -0700

    Update main.blocks, main.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -1,20 +1,7 @@
+namespace SpriteKind {
+    export const platform = SpriteKind.create()
+}
 scene.setBackgroundColor(3)
 let mySprite = sprites.create(assets.image`myImage0`, SpriteKind.Player)
-let mySprite2 = sprites.create(img`
-    . . . . . . . . . 3 . 6 6 6 6 6 
-    . . . . . . . . . 6 6 7 7 7 7 8 
-    . . . . . . 8 8 8 7 7 8 8 6 8 8 
-    . . e e e e c 6 6 8 8 . 8 7 8 . 
-    . e 2 5 4 2 e c 8 . . . 6 7 8 . 
-    e 2 4 2 2 2 2 2 c . . . 6 7 8 . 
-    e 2 2 2 2 2 2 2 c . . . 8 6 8 . 
-    e 2 e e 2 2 2 2 e e e e c 6 8 . 
-    c 2 e e 2 2 2 2 e 2 5 4 2 c 8 . 
-    . c 2 e e e 2 e 2 4 2 2 2 2 c . 
-    . . c 2 2 2 e e 2 2 2 2 2 2 2 e 
-    . . . e c c e c 2 2 2 2 2 2 2 e 
-    . . . . . . . c 2 e e 2 2 e 2 c 
-    . . . . . . . c e e e e e e 2 c 
-    . . . . . . . . c e 2 2 2 2 c . 
-    . . . . . . . . . c c c c c . . 
-    `, SpriteKind.Player)
+controller.moveSprite(mySprite)
+let mySprite2 = sprites.create(assets.image`myImage1`, SpriteKind.platform)
